f8d38a3236ebc2c9e557ec998bf254344286c39f
HonokaHarakawa <wow.hono313@gmail.com>
2022-10-12 13:56:48 +0000

    タブメニュー

---
+++ Tabbar-Lesson/js/script.js
@@ -0,0 +1,9 @@
+$('#tab_contents.tab[id !="tab1"]')
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
+});
